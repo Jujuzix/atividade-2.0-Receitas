4872a544273b8c260cd5123da6887941e5e0477b
Jujuzix <maronez.juliana@gmail.com>
2024-11-08 16:29:54 -0400

    Commit Final

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
 let cacheName = "Atividade-2.0-Receitas";
-let filesToCache = ["/", "/index.html", "index","pagCarnes.html","pagCarnes","pagDoces.html","pagDoces","pagMassas.html","pagMassas",
-                            "/receitasC/receita1.html","/receitasC/receita2.html", "/receitasC/receita3.html", "/receitasC/receita4.html",
-                            "/receitasC/receita5.html", "/receitasC/receita6.html","/receitasD/receita1.html","/receitasD/receita2.html",
-                            "/receitasD/receita3.html", "/receitasD/receita4.html", "/receitasD/receita5.html", "/receitasD/receita6.html",
-                            "/receitasM/receita1.html","/receitasM/receita2.html","/receitasM/receita3.html", "/receitasM/receita4.html",
+let filesToCache = ["/", "/index.html", "index","pagCarnes","pagDoces","pagMassas",
                             "/receitasC/receita1","/receitasC/receita2","/receitasC/receita3","/receitasC/receita4","/receitasC/receita5",
                             "/receitasC/receita6","/receitasD/receita1","/receitasD/receita2","/receitasD/receita3","/receitasD/receita4",
                             "/receitasD/receita5","/receitasD/receita6","/receitasM/receita1","/receitasM/receita2","/receitasM/receita3",
-                            "/receitasM/receita4","/receitasM/receita5","/receitasM/receita6","/receitasM/receita5.html", "/receitasM/receita6.html",
-                            "/css/style1.css","/css/style2.css", "/css/style3.css","/css/style4.css","/css/style5.css", "/js/main.js"];
+                            "/receitasM/receita4","/receitasM/receita5","/receitasM/receita6","/css/style1.css","/css/style2.css", "/css/style3.css","/css/style4.css","/css/style5.css", 
+                            "images/carne1.webp","images/carne2.webp","images/carne3.webp","images/carne4.webp","images/carne5.webp","images/carne6.webp","images/doce1.jpeg","images/doce2.webp",
+                            "images/doce3.webp","images/doce4.jpeg","images/doce5.webp","images/doce6.webp","images/logo.png","images/LogoPDM.png","images/massas1.webp","images/massas2.webp","images/massas3.jpg","images/massas4.webp","images/massas5.webp","images/massas6.webp","/js/main.js"];
 
 self.addEventListener("install", (e) => {
     e.waitUntil(
